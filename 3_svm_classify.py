dfd8a9c309fb9e08ee62f241770b76d32f0d6e23
modifyGB <2782299413@qq.com>
2022-01-27 21:47:52 +0800

    classify

--- 3_svm_classify.py
+++ 3_svm_classify.py
@@ -4,13 +4,13 @@ Version: 1.0
 Autor: Renhetian
 Date: 2022-01-27 21:11:05
 LastEditors: Renhetian
-LastEditTime: 2022-01-27 21:18:06
+LastEditTime: 2022-01-27 21:21:47
 '''
 
 from codes.DatasetLoader import DatasetLoader
 from codes.SVMClassification import SVMClassification
 
-# python -m script.svm_classify
+
 if __name__ == "__main__":
     dl = DatasetLoader('subjectivity')
     dl.load()
