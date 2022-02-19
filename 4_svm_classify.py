9adf2da129a034c67b97dc750297788fccd68769
modifyGB <2782299413@qq.com>
2022-02-20 00:59:24 +0800

    commit

--- 4_svm_classify.py
+++ 4_svm_classify.py
@@ -4,15 +4,18 @@ Version: 1.0
 Autor: Renhetian
 Date: 2022-01-27 21:11:05
 LastEditors: Renhetian
-LastEditTime: 2022-01-27 21:21:47
+LastEditTime: 2022-02-20 00:56:38
 '''
 
 from codes.DatasetLoader import DatasetLoader
 from codes.SVMClassification import SVMClassification
 
+cv = 10
+test_size = 0.3
+
 
 if __name__ == "__main__":
     dl = DatasetLoader('subjectivity')
     dl.load()
-    svm = SVMClassification(dl)
+    svm = SVMClassification(dl, cv=cv, test_size=test_size)
     svm.run()
